edf73d3a37fc8c8bf18c5c9e92f1b57cb8e88ae8
Jacob Webber <s1116548@gosforth.inf.ed.ac.uk>
2017-03-29 15:19:50 +0100

    added continuous integration and separate build files for run and test

---
+++ src/build_and_run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script installs a virtual env if it does not already exist, and builds and tests the server
+
+virtualenv env
+
+env/bin/pip install -r requirements.txt
+cd app
+../env/bin/yapf -i *.py
+../env/bin/nosetests && ../env/bin/python framework.py
